cb48e0ff5072ce205d1f97a59b7e3e4838509b57
Martin Schumacher <mschumacher@avides.com>
2020-01-04 20:22:36 +0100

    #2 - Support for java.sql.Connection

--- src/test/java/de/jaggl/sqlbuilder/queries/QueryTest.java
+++ src/test/java/de/jaggl/sqlbuilder/queries/QueryTest.java
@@ -4,13 +4,22 @@ import static de.jaggl.sqlbuilder.dialect.Dialects.MYSQL;
 import static de.jaggl.sqlbuilder.queries.Queries.select;
 import static de.jaggl.sqlbuilder.schema.Table.create;
 import static de.jaggl.sqlbuilder.utils.Indentation.enabled;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.easymock.EasyMock.expect;
+import static org.powermock.api.easymock.PowerMock.createStrictMock;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.verifyAll;
 
-import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-class QueryTest
+import org.junit.Test;
+
+public class QueryTest
 {
     @Test
-    void testPrintAndPrintln()
+    public void testPrintAndPrintln()
     {
         var query = select().from(create("table"));
 
@@ -51,5 +60,56 @@ class QueryTest
         query.print(System.out, "MYSQL", enabled());
         System.out.println();
         query.println(System.out, "MYSQL", enabled());
+    }
+
+    @Test
+    public void testPrepare() throws SQLException
+    {
+        var query = select().from(create("table"));
+
+        try (var connection = createStrictMock(Connection.class))
+        {
+            expect(connection.prepareStatement("SELECT * FROM `table`")).andReturn(createStrictMock(PreparedStatement.class));
+            connection.close();
+
+            replayAll();
+            assertThat(query.prepare(connection)).isInstanceOf(PreparedStatement.class);
+        }
+
+        verifyAll();
+    }
+
+    @Test
+    public void testPrepareWithDialect() throws SQLException
+    {
+        var query = select().from(create("table"));
+
+        try (var connection = createStrictMock(Connection.class))
+        {
+            expect(connection.prepareStatement("SELECT * FROM `table`")).andReturn(createStrictMock(PreparedStatement.class));
+            connection.close();
+
+            replayAll();
+            assertThat(query.prepare(connection, MYSQL)).isInstanceOf(PreparedStatement.class);
+        }
+
+        verifyAll();
+    }
+
+    @Test
+    public void testPrepareWithDialectName() throws SQLException
+    {
+        var query = select().from(create("table"));
+
+        try (var connection = createStrictMock(Connection.class))
+        {
+            expect(connection.prepareStatement("SELECT * FROM `table`")).andReturn(createStrictMock(PreparedStatement.class));
+            connection.close();
+
+            replayAll();
+            assertThat(query.prepare(connection, "MYSQL")).isInstanceOf(PreparedStatement.class);
+        }
+
+        verifyAll();
     }
 }
